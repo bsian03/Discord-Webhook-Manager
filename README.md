f073814287dd704f38794705de50a5576cd1649b
Bsian <bsian@staff.libraryofcode.us>
2019-11-27 20:04:35 +0000

    Files

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # Webhook Manager
+A Discord Webhook Manager for Node.JS - Easily customisable webhook settings with webhook rate limits and let the manager send the webhooks for you!
 
+## Installation
+Node 8+ is required for the webhook manager to work. Previous versions of Node.JS may work, however they will not be supported.
+To install, run this in your project folder:
+### NPM
+```
+npm i discord-webhook-manager
+```
+### Yarn
+```
+yarn add discord-webhook-manager
+```
+### Bower
+```
+bower install discord-webhook-manager
+```
+
+## Usage
+```js
+const WebhookManager = require('discord-webhook-manager');
+
+const webhook1 = new WebhookManager(webhookID, webhookToken[, format[, interval[, joinInputLengths]]]);
+webhook1.addToQueue('Message goes here')
+```
+`webhookID`: string - Webhook ID
+
+`webhookToken`: string - Webhook Token
+
+`format`: object (optional) - The way the webhook should be sent, this should be in the [Discord webhook format](https://discordapp.com/developers/docs/resources/webhook#execute-webhook). Use `'text'` as a placeholder for adding messages to the queue.  Default:
+```js
+{
+  content: 'text'
+}
+```
+
+`interval`: number (optional) - Interval at which webhooks should be sent at in milliseconds, e.g. `5000` would be 5 seconds. Default: `2000`
+
+`joinInputLengths`: number (optional) - Maximum character count of joined messages (especially useful for logs). If you don't want them joined, leave at `0`. Default: `0`
+
+## Properties/Functions
+### WebhookManager.url
+`webhook1.url`: string - URL of selected Webhook
+### WebhookManager.format
+`webhook1.format`: object - Format of webhook message as set in `format` parameter
+### WebhookManager.interval
+`webhook1.interval`: number - Interval at which webhooks are sent in milliseconds
+### WebhookManager.joinInputLengths
+`webhook1.joinInputLengths`: number - Maximum length of joined messages, or no joined messages if `0`
+### WebhookManager.queue
+`webhook1.queue`: string[] - Array of messages in the queue
+### WebhookManager.rateLimiter
+`webhook1.rateLimiter`: Date[] - Array of ISO8601 timestamps of sent webhooks, used for rate limiting webhook requests
+### WebhookManager.enabled
+`webhook1.enabled`: boolean - Whether the webhook is running or not
+### WebhookManager.addToQueue(message: string)
+`webhook1.addToQueue('Message here')`: string[] - Adds a message to the queue. Returns the queue
+### WebhookManager.emptyQueue()
+`webhook1.emptyQueue()`: string[] - Removes everything in the queue. Returns the empty queue
+### WebhookManager.resetRateLimit()
+`webhook1.resetRateLimit()`: Date[] - Resets the rate limit. Returns the empty rate limit
+### WebhookManager.updateWebhook(webhookID: string, webhookToken: string)
+`webhook1.updateWebhook('649305310778884106', 'TYCRyGQbze-UTFzlUgOmP6FgQK_0LnL5axCku06fTDr-CyI1VytbcDRveDl0frbA_PiM0')`: string - Updates the webhook URL. Returns the new Webhook URL (don't try using this, it doesn't exist)
+### WebhookManager.pauseWebhookSending()
+`webhook1.pauseWebhookSending()`: boolean - Pause webhook sending. Returns the enabled status
+### WebhookManager.startWebhookSending()
+`webhook1.startWebhookSending()`: boolean - Start/resume webhook sending. Returns the enabled status
+### WebhookManager.stopWebhookSending()
+`webhook1.stopWebhookSending()`: boolean - End webhook sending and clear the queue. Returns the enabled status
+
+## Events
+### error
+Emits when an error occurs with the webhook sending
+```js
+webhook1.on('error', (errorMessage: string, error: Error) => {
+  console.error(error)
+  console.error(errorMessage)
+  // Do something to resolve the error
+});
+```
---
+++ index.js
@@ -0,0 +1,164 @@
+const EventEmitter = require('events')
+const request = require('request-promise')
+
+/**
+ * Discord Webhook Manager
+ * @author Bsian <bsian@staff.libraryofcode.org>
+ * @note Initialise a new instance of the Webhook Manager for each webhook
+ */
+class WebhookManager extends EventEmitter {
+  /**
+   * Settings for Webhook Manager
+   * @param {string} webhookID Webhook ID
+   * @param {string} webhookToken Webhook Token
+   * @param {{content?: string, embeds?: {}[]}} format Format which the webhook should be sent in - use `'text'` as the wildcard phrase for where the logs should be placed.
+   * Default:
+   * ```js
+   * { content: 'text' }
+   * ```
+   * @param {number} interval Interval at which the webhooks should be sent at in milliseconds. Default: 2000
+   * @param {number} joinInputLengths Maximum length of joined messages - if you don't want them joined, select 0. Default: 0
+   */
+  constructor(webhookID, webhookToken, format = { content: 'text' }, interval = 2000, joinInputLengths = 0) {
+    super()
+    if (!webhookID || !webhookToken) throw new TypeError('Expected Webhook ID and Webhook Token')
+    if (interval < 0 || joinInputLengths < 0) throw new TypeError('`interval` and `joinInputLengths` parameter must be greater than or equal to 0')
+    this.url = `https://discordapp.com/api/webhooks/${webhookID}/${webhookToken}`
+    this.format = format
+    this.interval = interval
+    this.joinInputLengths = joinInputLengths
+    /**
+     * @type {string[]} Array of messages
+     */
+    this.queue = []
+    /**
+     * @type {Date[]}
+     */
+    this.rateLimiter = []
+    this.enabled = true
+
+    setInterval(() => { this.rateLimiter = this.rateLimiter.filter((d) => new Date(Date.now() - 2000) < d) })
+
+    setInterval(async () => {
+      if (this.rateLimiter.length >= 30 || !this.queue) return
+      const body = JSON.parse(JSON.stringify(this.format).replace(/text/g, this.queue[0]))
+      try {
+        await request({
+          method: 'POST', uri: this.url, body, json: true, resolveWithFullResponse: true
+        })
+        this.queue.shift()
+      } catch (error) {
+        if (error.statusCode !== 429 && error.statusCode !== 500) this.rateLimiter.push(new Date())
+        let errorMessage = `${error.response.statusCode} ${error.response.statusMessage}: ${error.error.code || error.statusCode} - ${error.error.message}`
+        if (error.statusCode === 429) errorMessage += '\nRate limits are not working, please open an issue at https://gitlab.libraryofcode.org/engineering/webhook-manager';
+        else if (error.statusCode === 401
+          || error.statusCode === 403
+          || error.statusCode === 404) {
+          errorMessage += '\nPlease try again with another webhook'
+          + '\nFor more info regarding webhooks, please see https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks';
+        }
+        return this.emit('error', [errorMessage, error])
+      }
+      this.rateLimiter.push(new Date())
+    }, this.interval)
+  }
+
+  /**
+   * Add a message to the queue
+   * @param {string} message Message to be added
+   * @returns {string[]} Current queue
+   */
+  addToQueue(message) {
+    if (!this.joinInputLengths) {
+      const splitMessages = this.splitString(message)
+      splitMessages.forEach((m) => this.queue.push(m))
+    } else {
+      const splitMessages = message.split('\n')
+      splitMessages.forEach((m) => {
+        const lastInQueue = this.queue[this.queue.length - 1]
+        if (lastInQueue.length + m.length > this.joinInputLengths) this.queue.push(m)
+        else lastInQueue += `\n${m}`
+      })
+    }
+    return this.queue
+  }
+
+  /**
+   * Removes everything in the queue
+   * @returns {string[]} Empty queue
+   */
+  emptyQueue() {
+    this.queue = []
+    return this.queue
+  }
+
+  /**
+   * Resets the rate limit
+   * @returns {Date[]} Empty rate limit
+   */
+  resetRateLimit() {
+    this.rateLimiter = []
+    return this.rateLimiter
+  }
+
+  /**
+   * Updates the webhook URL
+   * @param {string} webhookID New Webhook ID
+   * @param {string} webhookToken New Webhook Token
+   * @returns {string} New Webhook URL
+   */
+  updateWebhook(webhookID, webhookToken) {
+    this.url = `https://discordapp.com/api/webhooks/${webhookID}/${webhookToken}`
+    return this.url
+  }
+
+  /**
+   * Pause webhook sending
+   * @returns {boolean} Enabled status
+   */
+  pauseWebhookSending() {
+    this.enabled = false
+    return this.enabled
+  }
+
+  /**
+   * Start/resume webhook sending
+   * @returns {boolean} Enabled status
+   */
+  startWebhookSending() {
+    this.enabled = true
+    return this.enabled
+  }
+
+  /**
+   * End webhook sending and clear the queue
+   * @returns {boolean} Enabled status
+   */
+  stopWebhookSending() {
+    this.enabled = false
+    this.queue = []
+    return this.enabled
+  }
+
+  /**
+   * Split a message
+   * @param {string} message Message to be split
+   * @private
+   */
+  splitString(message) {
+    const msgArray = []
+    let str = ''
+    let pos = 0;
+    const maxLength = this.joinInputLengths || 2000
+    while (message.length > 0) {
+      pos = message.length > maxLength ? message.lastIndexOf('\n', maxLength) : message.length
+      if (pos > maxLength) pos = maxLength
+      str = message.substring(0, pos)
+      message = message.substring(pos)
+      msgArray.push(str)
+    }
+    return msgArray
+  }
+}
+
+module.exports = WebhookManager
